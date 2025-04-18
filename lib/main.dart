198bc76d86578d2074844283ebfcf635113ecc26
AsilMehaboob <138790394+AsilMehaboob@users.noreply.github.com>
2025-04-18 06:34:24 +0530

    name changed

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Ezygo Replica',
+      title: 'bunkr',
       initialRoute: '/',
       routes: {
         '/': (context) => const LoginScreen(),
