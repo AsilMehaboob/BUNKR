71ec55c4b7d30d8dd5cdf39f2ac1f585d7808839
AsilMehaboob <138790394+AsilMehaboob@users.noreply.github.com>
2025-05-30 15:02:53 +0530

    removed instituition card

--- lib/widgets/card.dart
+++ lib/widgets/card.dart
@@ -1,53 +1,31 @@
-import 'package:flutter/material.dart';
+import 'package:shadcn_ui/shadcn_ui.dart';
 
-class UserCard extends StatelessWidget {
-  const UserCard({super.key});
+enum NotifyAbout {
+  all,
+  mentions,
+  nothing;
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-      decoration: BoxDecoration(
-        color: Colors.black87, // dark background
-        borderRadius: BorderRadius.circular(8),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          // Name Section
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: const [
-              Text(
-                'Abhay',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Text(
-                'Balakrishnan',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ],
-          ),
-
-          // Icon Avatar
-          CircleAvatar(
-            radius: 20,
-            backgroundColor: Colors.white24,
-            child: Icon(
-              Icons.person,
-              color: Colors.white,
-            ),
-          ),
-        ],
-      ),
-    );
+  String get message {
+    return switch (this) {
+      all => 'All new messages',
+      mentions => 'Direct messages and mentions',
+      nothing => 'Nothing',
+    };
   }
 }
+
+ShadRadioGroupFormField<NotifyAbout>(
+  label: const Text('Notify me about'),
+  items: NotifyAbout.values.map(
+    (e) => ShadRadio(
+      value: e,
+      label: Text(e.message),
+    ),
+  ),
+  validator: (v) {
+    if (v == null) {
+      return 'You need to select a notification type.';
+    }
+    return null;
+  },
+),
