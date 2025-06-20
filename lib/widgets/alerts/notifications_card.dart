86d7d14f406814250ee8f0db59d4832841865df0
AsilMehaboob <138790394+AsilMehaboob@users.noreply.github.com>
2025-06-20 07:42:23 +0530

    changed notification expired time display logic

--- lib/widgets/alerts/notifications_card.dart
+++ lib/widgets/alerts/notifications_card.dart
@@ -31,13 +31,14 @@ class _CardNotificationsState extends State<CardNotifications> {
     final difference = now.difference(date);
 
     if (difference.inDays > 30) {
-      return '${(difference.inDays / 30).floor()} months ago';
+      final months = (difference.inDays / 30).floor();
+      return '$months month${months == 1 ? '' : 's'} ago';
     } else if (difference.inDays > 0) {
-      return '${difference.inDays} days ago';
+      return '${difference.inDays} day${difference.inDays == 1 ? '' : 's'} ago';
     } else if (difference.inHours > 0) {
-      return '${difference.inHours} hours ago';
+      return '${difference.inHours} hour${difference.inHours == 1 ? '' : 's'} ago';
     } else if (difference.inMinutes > 0) {
-      return '${difference.inMinutes} minutes ago';
+      return '${difference.inMinutes} minute${difference.inMinutes == 1 ? '' : 's'} ago';
     }
     return 'Just now';
   }
