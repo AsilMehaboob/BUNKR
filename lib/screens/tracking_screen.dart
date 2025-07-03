d4dc89435886320730228bace982ab1766802193
AsilMehaboob <138790394+AsilMehaboob@users.noreply.github.com>
2025-07-03 20:16:04 +0530

    added appbar in tracking screen

--- lib/screens/tracking_screen.dart
+++ lib/screens/tracking_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../widgets/appbar/app_bar.dart';
 
 class TrackingScreen extends StatelessWidget {
   const TrackingScreen({super.key});
@@ -6,6 +7,7 @@ class TrackingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: CustomAppBar(),
       body: Center(
         child: Text(
           'Tracking',
