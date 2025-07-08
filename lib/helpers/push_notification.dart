9d36038ca3e25c584995a52aa9c76b943b78a45d
AsilMehaboob <138790394+AsilMehaboob@users.noreply.github.com>
2025-07-08 13:52:08 +0530

    notification icon updated

--- lib/helpers/push_notification.dart
+++ lib/helpers/push_notification.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:flutter/material.dart';
 import '../firebase_options.dart';
 
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
@@ -85,6 +86,8 @@ class PushNotificationService {
               importance: Importance.max,
               priority: Priority.high,
               largeIcon: const DrawableResourceAndroidBitmap('logo'),
+              icon: '@drawable/mono_logo',
+              color: Colors.black,
             ),
           ),
         );
